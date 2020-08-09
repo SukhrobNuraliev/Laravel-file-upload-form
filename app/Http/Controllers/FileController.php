8695994853dea9583c427a168ed2d391b160f1a3
Sukhrob Nuraliev <sukhrobnuraliev@mail.ru>
2020-08-09 20:34:04 +0500

    initial commit

---
+++ app/Http/Controllers/FileController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\File;
+use Illuminate\Http\Request;
+
+class FileController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('welcome', ['images' => File::all()]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // if ($file = $request) {
+        //     $name = $file->file;
+        //     if ($file->move('images', $name)) {
+        //         $image = new File();
+        //         $image->image = $name;
+        //         $image->save();
+        //         return redirect('welcome');
+        //     };
+        // }
+
+        // return redirect()->back();
+        // dd($request->file);
+
+        if ($request->hasFile('file')) {
+            $name = $request->file->getClientOriginalName();
+            
+            $image = new File();
+            $image->image = $name;
+            $image->save();
+            
+            $request->file->storeAs('images', $name, 'public');
+        }
+
+        
+
+        return redirect()->back();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\File  $file
+     * @return \Illuminate\Http\Response
+     */
+    public function show(File $file)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\File  $file
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(File $file)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\File  $file
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, File $file)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\File  $file
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(File $file)
+    {
+        //
+    }
+}
